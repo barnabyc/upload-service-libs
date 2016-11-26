bb742bb96b4add45920fa743b64b862e4814cc65
Barnaby Claydon <barnaby@swipely.com>
2016-11-26 21:49:53 +0000

    Adding initial libs

---
+++ upload-model/upload.go
@@ -0,0 +1,15 @@
+package upload
+
+import (
+  // "io"
+  "fmt"
+)
+
+func Process(uuid []byte) {
+  fmt.Printf("upload.Process: %s\n", uuid)
+
+  fmt.Printf("todo: process the upload\n")
+  // todo: parse newly upload torrent
+}
+
+
