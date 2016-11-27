750878861bfef4959e8dddbe3cd16b28d449a98f
Barnaby Claydon <barnaby@swipely.com>
2016-11-27 22:48:52 +0000

    Testing torrent parsing predicates

--- upload-model/upload.go
+++ upload-model/upload.go
@@ -1,12 +1,33 @@
 package upload
 
 import (
-  // "io"
   "fmt"
+  "log"
+  "bytes"
+  "github.com/garyburd/redigo/redis"
+  // "github.com/swatkat/gotrntmetainfoparser"
 )
 
-func Process(uuid []byte) {
+func Process(uuid []byte, pool redis.Pool) {
   fmt.Printf("upload.Process: %s\n", uuid)
+
+  conn := pool.Get()
+  defer conn.Close()
+
+  n := bytes.IndexByte(uuid, 0)
+  uploadKey := string(uuid[:n])
+
+  uploadPath, err := redis.String(conn.Do("HGET", uploadKey, "path"))
+
+  if err != nil {
+    log.Printf("Error getting upload detail %s\n", err)
+    return
+  }
+
+  fmt.Printf("upload.Path: %s\n", uploadPath)
+
+  // MetaInfo::ReadTorrentMetaInfoFile( uploadPath )
+  // MetaInfo::DumpTorrentMetaInfo()
 
   fmt.Printf("todo: process the upload\n")
   // todo: parse newly upload torrent
