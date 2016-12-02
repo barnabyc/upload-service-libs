9d691c25af992f01c1349863454a9d1f57f8338a
Barnaby Claydon <barnaby@swipely.com>
2016-12-01 20:16:35 -0500

    Update with debug detail

--- upload-model/upload.go
+++ upload-model/upload.go
@@ -2,6 +2,9 @@ package upload
 
 import (
   "fmt"
+  "os"
+  "path/filepath"
+  "github.com/jackpal/bencode-go"
   "github.com/garyburd/redigo/redis"
   "github.com/swatkat/gotrntmetainfoparser"
 )
@@ -23,10 +26,37 @@ func Process(uuid []byte, pool *redis.Pool) {
 
   mi := gotrntmetainfoparser.MetaInfo{}
 
+  fmt.Printf("debug: upload file ext: %s\n", filepath.Ext(uploadPath))
+
+  file, er := os.Open(uploadPath)
+  if er != nil {
+    fmt.Printf("debug: could not open file: %s\n", er)
+    return
+  }
+  defer file.Close()
+
+  // Decode bencoded metainfo file.
+  fileMetaData, er := bencode.Decode(file)
+  if er != nil {
+    fmt.Printf("debug: could not decode file: %s\n", er)
+    return
+  }
+
+  // fileMetaData is map of maps of... maps. Get top level map.
+  _, ok := fileMetaData.(map[string]interface{})
+  if !ok {
+    fmt.Printf("debug: could not get top level map\n")
+    return
+  }
+
+
+
+
+
   if mi.ReadTorrentMetaInfoFile( uploadPath ) {
     mi.DumpTorrentMetaInfo()
   } else {
-    fmt.Printf("error: could not parse upload")
+    fmt.Printf("error: could not parse upload\n")
   }
 }
 
