f11507f47e75ebe87bb2f1199dd912c15d4a735a
Barnaby Claydon <barnaby@swipely.com>
2017-06-24 17:49:48 -0400

    Start reworking gotrntmetainfoparser

--- upload-model/upload.go
+++ upload-model/upload.go
@@ -2,12 +2,47 @@ package upload
 
 import (
   "bytes"
+  "crypto/sha1"
   "fmt"
   "github.com/garyburd/redigo/redis"
   "github.com/jackpal/bencode-go"
   "github.com/oklog/ulid"
-  "github.com/swatkat/gotrntmetainfoparser" // todo: fork and clean-up
+  "time"
+  // "github.com/swatkat/gotrntmetainfoparser" // todo: fork and clean-up
 )
+
+// Structs into which torrent metafile is
+// parsed and stored into.
+type FileDict struct {
+  Length int64    "length"
+  Path   []string "path"
+  Md5sum string   "md5sum"
+}
+
+type InfoDict struct {
+  FileDuration []int64 "file-duration"
+  FileMedia    []int64 "file-media"
+  // Single file
+  Name   string "name"
+  Length int64  "length"
+  Md5sum string "md5sum"
+  // Multiple files
+  Files       []FileDict "files"
+  PieceLength int64      "piece length"
+  Pieces      string     "pieces"
+  Private     int64      "private"
+}
+
+type MetaInfo struct {
+  Info         InfoDict   "info"
+  InfoHash     string     "info hash"
+  Announce     string     "announce"
+  AnnounceList [][]string "announce-list"
+  CreationDate int64      "creation date"
+  Comment      string     "comment"
+  CreatedBy    string     "created by"
+  Encoding     string     "encoding"
+}
 
 func Process(id ulid.ULID, pool *redis.Pool) {
   fmt.Printf("upload.Process: %s\n", id)
@@ -22,27 +57,116 @@ func Process(id ulid.ULID, pool *redis.Pool) {
     return
   }
 
-  mi := gotrntmetainfoparser.MetaInfo{}
+  mi := MetaInfo{}
 
   buf := bytes.NewBufferString(file)
 
-  // Decode bencoded metainfo file.
-  fileMetaData, er := bencode.Decode(buf)
-  if er != nil {
-    fmt.Printf("debug: could not decode file: %s\n", er)
-    return
-  }
-
-  // fileMetaData is map of maps of... maps. Get top level map.
-  _, ok := fileMetaData.(map[string]interface{})
-  if !ok {
-    fmt.Printf("debug: could not get top level map\n")
-    return
-  }
-
-  if mi.ReadTorrentMetaInfoFile(buf) {
+  if mi.ReadTorrentMetaInfo(buf) {
     mi.DumpTorrentMetaInfo()
   } else {
     fmt.Printf("error: could not parse upload\n")
   }
+}
+
+func (metaInfo *MetaInfo) ReadTorrentMetaInfo(buffer *bytes.Buffer) bool {
+  // Decode bencoded metainfo file.
+  fileMetaData, er := bencode.Decode(buffer)
+  if er != nil {
+    fmt.Printf("debug: could not decode file: %s\n", er)
+    return false
+  }
+
+  // fileMetaData is map of maps of... maps. Get top level map.
+  metaInfoMap, ok := fileMetaData.(map[string]interface{})
+  if !ok {
+    fmt.Printf("debug: could not get top level map\n")
+    return false
+  }
+
+  // Enumerate through child maps.
+  var bytesBuf bytes.Buffer
+  for mapKey, mapVal := range metaInfoMap {
+    switch mapKey {
+    case "info":
+      if er = bencode.Marshal(&bytesBuf, mapVal); er != nil {
+        return false
+      }
+
+      infoHash := sha1.New()
+      infoHash.Write(bytesBuf.Bytes())
+      metaInfo.InfoHash = string(infoHash.Sum(nil))
+
+      if er = bencode.Unmarshal(&bytesBuf, &metaInfo.Info); er != nil {
+        return false
+      }
+
+    case "announce-list":
+      if er = bencode.Marshal(&bytesBuf, mapVal); er != nil {
+        return false
+      }
+      if er = bencode.Unmarshal(&bytesBuf, &metaInfo.AnnounceList); er != nil {
+        return false
+      }
+
+    case "announce":
+      metaInfo.Announce = mapVal.(string)
+
+    case "creation date":
+      metaInfo.CreationDate = mapVal.(int64)
+
+    case "comment":
+      metaInfo.Comment = mapVal.(string)
+
+    case "created by":
+      metaInfo.CreatedBy = mapVal.(string)
+
+    case "encoding":
+      metaInfo.Encoding = mapVal.(string)
+    }
+  }
+
+  return true
+}
+
+// Print torrent meta info struct data.
+func (metaInfo *MetaInfo) DumpTorrentMetaInfo() {
+  fmt.Println("Announce:", metaInfo.Announce)
+  fmt.Println("Announce List:")
+  for _, anncListEntry := range metaInfo.AnnounceList {
+    for _, elem := range anncListEntry {
+      fmt.Println("    ", elem)
+    }
+  }
+  strCreationDate := time.Unix(metaInfo.CreationDate, 0)
+  fmt.Println("Creation Date:", strCreationDate)
+  fmt.Println("Comment:", metaInfo.Comment)
+  fmt.Println("Created By:", metaInfo.CreatedBy)
+  fmt.Println("Encoding:", metaInfo.Encoding)
+  fmt.Printf("InfoHash: %X\n", metaInfo.InfoHash)
+  fmt.Println("Info:")
+  fmt.Println("    Piece Length:", metaInfo.Info.PieceLength)
+  piecesList := metaInfo.getPiecesList()
+  fmt.Printf("    Pieces:%X -- %X\n", len(piecesList), len(metaInfo.Info.Pieces)/20)
+  fmt.Println("    File Duration:", metaInfo.Info.FileDuration)
+  fmt.Println("    File Media:", metaInfo.Info.FileMedia)
+  fmt.Println("    Private:", metaInfo.Info.Private)
+  fmt.Println("    Name:", metaInfo.Info.Name)
+  fmt.Println("    Length:", metaInfo.Info.Length)
+  fmt.Println("    Md5sum:", metaInfo.Info.Md5sum)
+  fmt.Println("    Files:")
+  for _, fileDict := range metaInfo.Info.Files {
+    fmt.Println("        Length:", fileDict.Length)
+    fmt.Println("        Path:", fileDict.Path)
+    fmt.Println("        Md5sum:", fileDict.Md5sum)
+  }
+}
+
+// Splits pieces string into an array of 20 byte SHA1 hashes.
+func (metaInfo *MetaInfo) getPiecesList() []string {
+  var piecesList []string
+  piecesLen := len(metaInfo.Info.Pieces)
+  for i, j := 0, 0; i < piecesLen; i, j = i+20, j+1 {
+    piecesList = append(piecesList, metaInfo.Info.Pieces[i:i+19])
+  }
+  return piecesList
 }
